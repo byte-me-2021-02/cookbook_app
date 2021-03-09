35f61d96be546af5deebf214765985e3b6b1932c
brian <riceb53@gmail.com>
2021-03-08 21:31:48 -0500

    add signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,12 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    # index shows ALL recipes
     get "/recipes" => "recipes#index"
-    # show shows ONE recipe
     get "/recipes/:id" => "recipes#show"
-    # create a new recipe in the db
     post "/recipes" => "recipes#create"
-    # update existing recipe
     patch "/recipes/:id" => "recipes#update"
-    # delete existing recipe
     delete "/recipes/:id" => "recipes#destroy"
+
+    post "/users" => "users#create"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_010900) do
+ActiveRecord::Schema.define(version: 2021_03_09_022000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2021_02_23_010900) do
     t.integer "prep_time"
     t.string "image_url"
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
